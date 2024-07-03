04df4b2ee77ea4844a49bd80ee272d75203937b9
Ktumsh <jbz.january@gmail.com>
2024-07-02 20:57:29 -0400

    .

--- create_profiles_for_existing_users.py
+++
@@ -1,7 +0,0 @@
-# create_profiles_for_existing_users.py
-from django.contrib.auth.models import User
-from store.models import Profile
-
-for user in User.objects.all():
-    if not hasattr(user, 'profile'):
-        Profile.objects.create(user=user)
